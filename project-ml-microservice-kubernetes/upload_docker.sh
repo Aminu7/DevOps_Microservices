a0c5778c547eaab4229359744094e36cd0e61395
Ubuntu <ubuntu@ip-172-31-0-50.ec2.internal>
2022-08-26 07:34:15 +0000

    project enviroment created

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="aminu/flaskapp:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag  aminu/flaskapp:v1.0.0  $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push aminu/flaskapp:v1.0.0
